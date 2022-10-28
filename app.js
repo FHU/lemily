ad650e30451f518752acdf065f50924b9714ca5f
LTsoccer <77810494+LTsoccer@users.noreply.github.com>
2022-10-28 00:38:50 -0500

    stats work

--- app.js
+++ app.js
@@ -5,6 +5,35 @@ const box = document.querySelectorAll(".box2");
 window.addEventListener( 'scroll', checkboxes);
 window.addEventListener( 'scroll', checkbox);
 
+const circle1 = document.querySelector(".one");
+circle1.addEventListener('click', function() {
+    circle1.children[0].classList.toggle("hidden");
+    circle1.children[1].classList.toggle("hidden");
+});
+
+const circle2 = document.querySelector(".two");
+circle2.addEventListener('click', function() {
+    circle2.children[0].classList.toggle("hidden");
+    circle2.children[1].classList.toggle("hidden");
+});
+
+const circle3 = document.querySelector(".three");
+circle3.addEventListener('click', function() {
+    circle3.children[0].classList.toggle("hidden");
+    circle3.children[1].classList.toggle("hidden");
+});
+
+const circle4 = document.querySelector(".four");
+circle4.addEventListener('click', function() {
+    circle4.children[0].classList.toggle("hidden");
+    circle4.children[1].classList.toggle("hidden");
+});
+
+const circle5 = document.querySelector(".five");
+circle5.addEventListener('click', function() {
+    circle5.children[0].classList.toggle("hidden");
+    circle5.children[1].classList.toggle("hidden");
+});
 
 function checkboxes() {
     const triggerBottom = window.innerHeight / 5 * 4;
